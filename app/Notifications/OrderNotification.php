c99a31feab6cdcd96604c08bd58bb38393ef9a5d
SWEETIS <abalan1510it@gmail.com>
2023-01-13 21:44:17 +0200

    send notification telegram

--- app/Notifications/OrderNotification.php
+++ app/Notifications/OrderNotification.php
@@ -4,6 +4,7 @@ namespace App\Notifications;
 
 use Akaunting\Money\Currency;
 use Akaunting\Money\Money;
+use App\Models\TelegramUser;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -15,6 +16,8 @@ use NotificationChannels\Twilio\TwilioChannel;
 use NotificationChannels\Twilio\TwilioSmsMessage;
 use App\NotificationChannels\Expo\ExpoChannel;
 use App\NotificationChannels\Expo\ExpoMessage;
+use App\Tables;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class OrderNotification extends Notification
 {
@@ -29,7 +32,7 @@ class OrderNotification extends Notification
     protected $status;
     protected $user;
 
-    public function __construct($order, $status = '1',$user=null)
+    public function __construct($order, $status = '1', $user = null)
     {
         $this->order = $order;
         $this->status = $status;
@@ -45,11 +48,11 @@ class OrderNotification extends Notification
     public function via($notifiable)
     {
         $notificationClasses = ['database'];
-        
+
         //Mail notification on vendor email
-        if($this->order->restorant->getConfig('enable_email_order_notification',false)){
+        if ($this->order->restorant->getConfig('enable_email_order_notification', false)) {
             array_push($notificationClasses, 'mail');
-        }else if(config('settings.send_order_email_to_vendor',false)){
+        } else if (config('settings.send_order_email_to_vendor', false)) {
             array_push($notificationClasses, 'mail');
         }
 
@@ -57,47 +60,46 @@ class OrderNotification extends Notification
             array_push($notificationClasses, OneSignalChannel::class);
         }
         if (config('settings.twilio_account_sid') && config('settings.send_sms_notifications')) {
-            if( $this->order->client&&strlen($this->order->client->phone)>4){
+            if ($this->order->client && strlen($this->order->client->phone) > 4) {
                 array_push($notificationClasses, TwilioChannel::class);
             }
         }
-        if($this->user!=null&&strlen($this->user->expotoken)>3){
-            array_push($notificationClasses,ExpoChannel::class);  
+        if ($this->user != null && strlen($this->user->expotoken) > 3) {
+            array_push($notificationClasses, ExpoChannel::class);
         }
         return $notificationClasses;
     }
 
     public function toExpo($notifiable)
     {
-        $messages=$this->getMessages();
-        $greeting=$messages[0];
-        $line=$messages[1];
+        $messages = $this->getMessages();
+        $greeting = $messages[0];
+        $line = $messages[1];
         try {
             return ExpoMessage::create()
-            ->title($greeting)
-            ->body($line)
-            ->badge(1);
+                ->title($greeting)
+                ->body($line)
+                ->badge(1);
         } catch (\Throwable $th) {
             //throw $th;
         }
-        
     }
 
     public function toTwilio($notifiable)
     {
-        if ($this->status.'' == '1') {
+        if ($this->status . '' == '1') {
             //Created
-             $line = $this->order->delivery_method.'' == '3'? __('You have just received an order on table').' '.$this->order->table->name :  __('You have just received an order');
-        } elseif ($this->status.'' == '3') {
+            $line = $this->order->delivery_method . '' == '3' ? __('You have just received an order on table') . ' ' . $this->order->table->name :  __('You have just received an order');
+        } elseif ($this->status . '' == '3') {
             //Accepted
             $line = __('Your order has been accepted. We are now working on it!');
-        } elseif ($this->status.'' == '4') {
+        } elseif ($this->status . '' == '4') {
             //Assigned to driver
             $line = __('There is new order assigned to you.');
-        } elseif ($this->status.'' == '5') {
+        } elseif ($this->status . '' == '5') {
             //Prepared
-            $line = $this->order->delivery_method && $this->order->delivery_method.'' == '1' ? __('Your order is ready for delivery. Expect us soon.') : __('Your order is ready for pickup. We are expecting you.');
-        } elseif ($this->status.'' == '9') {
+            $line = $this->order->delivery_method && $this->order->delivery_method . '' == '1' ? __('Your order is ready for delivery. Expect us soon.') : __('Your order is ready for pickup. We are expecting you.');
+        } elseif ($this->status . '' == '9') {
             //Rejected
             $line = __('Unfortunately your order is rejected. There where issues with the order and we need to reject it. Pls contact us for more info.');
         }
@@ -105,38 +107,39 @@ class OrderNotification extends Notification
         return (new TwilioSmsMessage())->content($line);
     }
 
-    private function getMessages(){
-        if ($this->status.'' == '1') {
+    private function getMessages()
+    {
+        if ($this->status . '' == '1') {
             //Created
             $greeting = __('There is new order');
-            $line = $this->order->delivery_method.'' == '3'? __('You have just received an order on table').' '.$this->order->table->name :  __('You have just received an order');
-        } elseif ($this->status.'' == '3') {
+            $line = $this->order->delivery_method . '' == '3' ? __('You have just received an order on table') . ' ' . $this->order->table->name :  __('You have just received an order');
+        } elseif ($this->status . '' == '3') {
             //Accepted
             $greeting = __('Your order has been accepted');
             $line = __('We are now working on it!');
-        } elseif ($this->status.'' == '4') {
+        } elseif ($this->status . '' == '4') {
             //Assigned to driver
             $greeting = __('There is new order for you.');
             $line = __('There is new order assigned to you.');
-        } elseif ($this->status.'' == '5') {
+        } elseif ($this->status . '' == '5') {
             //Prepared
             $greeting = __('Your order is ready.');
-            $line = $this->order->delivery_method && $this->order->delivery_method.'' == '1' ? __('Your order is ready for delivery. Expect us soon.') : __('Your order is ready for pickup. We are expecting you.');
-        } elseif ($this->status.'' == '9') {
+            $line = $this->order->delivery_method && $this->order->delivery_method . '' == '1' ? __('Your order is ready for delivery. Expect us soon.') : __('Your order is ready for pickup. We are expecting you.');
+        } elseif ($this->status . '' == '9') {
             //Rejected
             $greeting = __('Order rejected');
             $line = __('Unfortunately your order is rejected. There where issues with the order and we need to reject it. Pls contact us for more info.');
         }
-        return [$greeting." #".$this->order->id,$line];
+        return [$greeting . " #" . $this->order->id, $line];
     }
 
     public function toOneSignal($notifiable)
     {
-        $messages=$this->getMessages();
-        $greeting=$messages[0];
-        $line=$messages[1];
+        $messages = $this->getMessages();
+        $greeting = $messages[0];
+        $line = $messages[1];
 
-        $url = url('/orders/'.$this->order->id);
+        $url = url('/orders/' . $this->order->id);
 
         //Inders in the db
 
@@ -161,25 +164,25 @@ class OrderNotification extends Notification
     public function toMail($notifiable)
     {
         //Change currency
-        \App\Services\ConfChanger::switchCurrency( $this->order->restorant);
+        \App\Services\ConfChanger::switchCurrency($this->order->restorant);
 
-        if ($this->status.'' == '1') {
+        if ($this->status . '' == '1') {
             //Created
             $greeting = __('There is new order');
-            $line = $this->order->delivery_method.'' == '3'? __('You have just received an order on table').' '.$this->order->table->name :  __('You have just received an order');
-        } elseif ($this->status.'' == '3') {
+            $line = $this->order->delivery_method . '' == '3' ? __('You have just received an order on table') . ' ' . $this->order->table->name :  __('You have just received an order');
+        } elseif ($this->status . '' == '3') {
             //Accepted
             $greeting = __('Your order has been accepted');
             $line = __('We are now working on it!');
-        } elseif ($this->status.'' == '4') {
+        } elseif ($this->status . '' == '4') {
             //Assigned to driver
             $greeting = __('There is new order for you.');
             $line = __('There is new order assigned to you.');
-        } elseif ($this->status.'' == '5') {
+        } elseif ($this->status . '' == '5') {
             //Prepared
             $greeting = __('Your order is ready.');
-            $line = $this->order->delivery_method && $this->order->delivery_method.'' == '1' ? __('Your order is ready for delivery. Expect us soon.') : __('Your order is ready for pickup. We are expecting you.');
-        } elseif ($this->status.'' == '9') {
+            $line = $this->order->delivery_method && $this->order->delivery_method . '' == '1' ? __('Your order is ready for delivery. Expect us soon.') : __('Your order is ready for pickup. We are expecting you.');
+        } elseif ($this->status . '' == '9') {
             //Rejected
             $greeting = __('Order rejected');
             $line = __('Unfortunately your order is rejected. There where issues with the order and we need to reject it. Pls contact us for more info.');
@@ -187,29 +190,29 @@ class OrderNotification extends Notification
 
         $message = (new MailMessage)
             ->greeting($greeting)
-            ->subject(__('Order notification').' #'.$this->order->id)
+            ->subject(__('Order notification') . ' #' . $this->order->id)
             ->line($line)
-            ->action(__('View Order'), url('/orders/'.$this->order->id));
+            ->action(__('View Order'), url('/orders/' . $this->order->id));
 
         //Add order details
         $message->line(__('Order items'));
         $message->line(__('________________'));
         foreach ($this->order->items as $key => $item) {
-            $lineprice = $item->pivot->qty.' X '.$item->name.$item->pivot->variant_name.' ( '.money($item->pivot->variant_price, config('settings.cashier_currency'), config('settings.do_convertion')).' ) = '.money($item->pivot->qty * $item->pivot->variant_price, config('settings.cashier_currency'), true);
+            $lineprice = $item->pivot->qty . ' X ' . $item->name . $item->pivot->variant_name . ' ( ' . money($item->pivot->variant_price, config('settings.cashier_currency'), config('settings.do_convertion')) . ' ) = ' . money($item->pivot->qty * $item->pivot->variant_price, config('settings.cashier_currency'), true);
             $message->line($lineprice);
         }
         $message->line(__('________________'));
-        $message->line(__('Sub Total').': '.money($this->order->order_price, config('settings.cashier_currency'), config('settings.do_convertion')));
+        $message->line(__('Sub Total') . ': ' . money($this->order->order_price, config('settings.cashier_currency'), config('settings.do_convertion')));
 
-        if ($this->order->delivery_method && $this->order->delivery_method.'' == '1') {
-            $message->line(__('Delivery').': '.money($this->order->delivery_price, config('settings.cashier_currency'), config('settings.do_convertion')));
+        if ($this->order->delivery_method && $this->order->delivery_method . '' == '1') {
+            $message->line(__('Delivery') . ': ' . money($this->order->delivery_price, config('settings.cashier_currency'), config('settings.do_convertion')));
         }
 
-        if ($this->order->discount>0) {
-            $message->line(__('Discount').': '.money($this->order->discount, config('settings.cashier_currency'), config('settings.do_convertion')));
+        if ($this->order->discount > 0) {
+            $message->line(__('Discount') . ': ' . money($this->order->discount, config('settings.cashier_currency'), config('settings.do_convertion')));
         }
 
-        $message->line(__('Total').': '.money($this->order->order_price_with_discount+$this->order->delivery_price, config('settings.cashier_currency'), config('settings.do_convertion')));
+        $message->line(__('Total') . ': ' . money($this->order->order_price_with_discount + $this->order->delivery_price, config('settings.cashier_currency'), config('settings.do_convertion')));
 
         return $message;
     }
@@ -222,36 +225,52 @@ class OrderNotification extends Notification
      */
     public function toArray($notifiable)
     {
-        return [ ];
+        return [];
     }
 
     public function toDatabase($notifiable)
     {
-        if ($this->status.'' == '1') {
+        if ($this->status . '' == '1') {
             //Created
             $greeting = __('There is new order');
             $line = __('You have just received an order');
-        } elseif ($this->status.'' == '3') {
+        } elseif ($this->status . '' == '3') {
             //Accepted
             $greeting = __('Your order has been accepted');
-            $line = __('order').'#'.$this->order->id.' '.__('We are now working on it!');
-        } elseif ($this->status.'' == '4') {
+            $line = __('order') . '#' . $this->order->id . ' ' . __('We are now working on it!');
+        } elseif ($this->status . '' == '4') {
             //Assigned to driver
             $greeting = __('There is new order for you.');
             $line = __('There is new order assigned to you.');
-        } elseif ($this->status.'' == '5') {
+        } elseif ($this->status . '' == '5') {
             //Prepared
             $greeting = __('Your order is ready.');
-            $line = $this->order->delivery_method && $this->order->delivery_method.'' == '1' ? __('Your order is ready for delivery. Expect us soon.') : __('Your order is ready for pickup. We are expecting you.');
-        } elseif ($this->status.'' == '9') {
+            $line = $this->order->delivery_method && $this->order->delivery_method . '' == '1' ? __('Your order is ready for delivery. Expect us soon.') : __('Your order is ready for pickup. We are expecting you.');
+        } elseif ($this->status . '' == '9') {
             //Rejected
             $greeting = __('Order rejected');
             $line = __('Unfortunately your order is rejected. There where issues with the order and we need to reject it. Pls contact us for more info.');
         }
 
         return [
-            'title'=>$greeting,
-            'body' =>$line,
+            'title' => $greeting,
+            'body' => $line,
         ];
+    }
+
+    public function toTelegram()
+    {
+        $table = Tables::find($this->order->table_id);
+        $restoarea = $table->restoarea()->first();
+        $telegramUser = TelegramUser::where('user_id', $restoarea->user_id)->firstOrFail();
+        $oraComenzii = date('H:i', strtotime($this->order->created_at));
+
+        $message = "🚀 *Comanda Noua!*\n\nZona: *{$restoarea->name}*\nMasa: *{$table->name}*\nOra Comenzii : *{$oraComenzii}*";
+
+        Telegram::sendMessage([
+            'chat_id' => (string) $telegramUser->chat_id,
+            'text' => $message,
+            'parse_mode' => 'Markdown',
+        ]);
     }
 }
--- app/Repositories/Orders/BaseOrderRepository.php
+++ app/Repositories/Orders/BaseOrderRepository.php
@@ -50,27 +50,27 @@ class BaseOrderRepository extends Controller
     /**
      * @var bool status
      */
-    public $status=true;
+    public $status = true;
 
     /**
      * @var bool isNewOrder
      */
-    public $isNewOrder=true;
+    public $isNewOrder = true;
 
     /**
      * @var string errorMessage - Deliver, DineIn, PickUp
      */
-    public $errorMessage="";
+    public $errorMessage = "";
 
     /**
      * @var Redirect paymentRedirect
      */
-    public $paymentRedirect=null;
+    public $paymentRedirect = null;
 
-     /**
+    /**
      * @var bool isMobileOrder
      */
-    public $isMobileOrder=false;
+    public $isMobileOrder = false;
 
 
     /**
@@ -78,25 +78,26 @@ class BaseOrderRepository extends Controller
      */
     public $redirectLink;
 
-    public function __construct($vendor_id,$request,$expedition,$hasPayment,$isStripe){
-        $this->request=$request;
-        $this->expedition=$expedition;
-        $this->hasPayment=$hasPayment;
-        $this->isStripe=$isStripe;
+    public function __construct($vendor_id, $request, $expedition, $hasPayment, $isStripe)
+    {
+        $this->request = $request;
+        $this->expedition = $expedition;
+        $this->hasPayment = $hasPayment;
+        $this->isStripe = $isStripe;
 
         //Set the Vendor
-        if($vendor_id){
+        if ($vendor_id) {
             $this->vendor = Vendor::findOrFail($vendor_id);
-        }else{
-            $this->vendor=null;
+        } else {
+            $this->vendor = null;
         }
-       
     }
 
-    
 
-    public function constructOrder(){
-        //Create the order 
+
+    public function constructOrder()
+    {
+        //Create the order
         $this->createOrder();
 
         //Set Items
@@ -107,80 +108,83 @@ class BaseOrderRepository extends Controller
 
         //Calculate fees
         $this->calculateFees();
-
     }
 
-    public function validateOrder(){
-        $validator = Validator::make(['order_price'=>$this->order->order_price], [
-            'order_price'=>['numeric','min:0'.$this->vendor->minimum]
+    public function validateOrder()
+    {
+        $validator = Validator::make(['order_price' => $this->order->order_price], [
+            'order_price' => ['numeric', 'min:0' . $this->vendor->minimum]
         ]);
-        if($validator->fails()){
+        if ($validator->fails()) {
             $this->invalidateOrder();
         }
         return $validator;
     }
 
-    public function invalidateOrder(){
-        $this->status=false;
+    public function invalidateOrder()
+    {
+        $this->status = false;
         $this->order->delete();
     }
 
-    public function updateOrder(){
+    public function updateOrder()
+    {
         //Store it if not stored yet, otherwise update it
         $this->order->update();
     }
 
-    public function finalizeOrder(){
+    public function finalizeOrder()
+    {
     }
 
-    private function createOrder(){
-        if($this->order==null){
-            $this->order=new Order;
-            $this->order->restorant_id=$this->vendor?$this->vendor->id:null;
-            $this->order->comment="";
-            $this->order->payment_method=$this->request->payment_method;
-            $this->order->payment_status="unpaid";
+    private function createOrder()
+    {
+        if ($this->order == null) {
+            $this->order = new Order;
+            $this->order->restorant_id = $this->vendor ? $this->vendor->id : null;
+            $this->order->comment = "";
+            $this->order->payment_method = $this->request->payment_method;
+            $this->order->payment_status = "unpaid";
 
-            $expeditionsTypes=['delivery'=>1,'pickup'=>2,'dinein'=>3]; //1- delivery 2 - pickup 3-dinein
-            $this->order->delivery_method=$expeditionsTypes[$this->expedition];  
+            $expeditionsTypes = ['delivery' => 1, 'pickup' => 2, 'dinein' => 3]; //1- delivery 2 - pickup 3-dinein
+            $this->order->delivery_method = $expeditionsTypes[$this->expedition];
 
             //Client
-            if(auth()->user()){
-                $this->order->client_id=auth()->user()->id;
+            if (auth()->user()) {
+                $this->order->client_id = auth()->user()->id;
             }
 
-            $this->order->order_price=0;
-            $this->order->vatvalue=0;
+            $this->order->order_price = 0;
+            $this->order->vatvalue = 0;
 
             //Save order
             $this->order->save();
 
-            $this->order->md=md5($this->order->id);
+            $this->order->md = md5($this->order->id);
             $this->order->update();
 
- 
+
 
             //Save order custom fields
             $this->order->setMultipleConfig($this->request->customFields);
-
-
-        }else{
+        } else {
             //Order is already initialized - in case of continues ordering
-            $this->isNewOrder=false;
+            $this->isNewOrder = false;
         }
     }
-    
-    private function setItems(){
+
+    private function setItems()
+    {
 
         foreach ($this->request->items as $key => $item) {
 
-            
+
             //Obtain the item
             $theItem = Items::findOrFail($item['id']);
 
             //List of extras
             $extras = [];
-            
+
             //The price of the item or variant
             $itemSelectedPrice = $theItem->price;
 
@@ -193,88 +197,94 @@ class BaseOrderRepository extends Controller
                 $variantName = $variant->optionsList;
             }
 
-           //Find the extras
+            //Find the extras
             foreach ($item['extrasSelected'] as $key => $extra) {
                 $theExtra = $theItem->extras()->findOrFail($extra['id']);
-                $itemSelectedPrice+=$theExtra->price;
-                array_push($extras, $theExtra->name.' + '.money($theExtra->price, config('settings.cashier_currency'), config('settings.do_convertion')));
+                $itemSelectedPrice += $theExtra->price;
+                array_push($extras, $theExtra->name . ' + ' . money($theExtra->price, config('settings.cashier_currency'), config('settings.do_convertion')));
             }
-            
+
             //Total vat on this item
-            $totalCalculatedVAT = $item['qty'] * ($theItem->vat > 0?$itemSelectedPrice * ($theItem->vat / 100):0);
+            $totalCalculatedVAT = $item['qty'] * ($theItem->vat > 0 ? $itemSelectedPrice * ($theItem->vat / 100) : 0);
 
             $this->order->items()->attach($item['id'], [
-                'qty'=>$item['qty'], 
-                'extras'=>json_encode($extras), 
-                'vat'=>$theItem->vat, 
-                'vatvalue'=>$totalCalculatedVAT, 
-                'variant_name'=>$variantName, 
-                'variant_price'=>$itemSelectedPrice
+                'qty' => $item['qty'],
+                'extras' => json_encode($extras),
+                'vat' => $theItem->vat,
+                'vatvalue' => $totalCalculatedVAT,
+                'variant_name' => $variantName,
+                'variant_price' => $itemSelectedPrice
             ]);
-        } 
+        }
 
 
         //After we have updated the list of items, we need to update the order price
-        $order_price=0;
-        $total_order_vat=0;
+        $order_price = 0;
+        $total_order_vat = 0;
         foreach ($this->order->items()->get() as $key => $item) {
-            $order_price+=$item->pivot->qty*$item->pivot->variant_price;
-            $total_order_vat+=$item->pivot->vatvalue;
+            $order_price += $item->pivot->qty * $item->pivot->variant_price;
+            $total_order_vat += $item->pivot->vatvalue;
         }
-        $this->order->order_price=$order_price;
-        $this->order->vatvalue=$total_order_vat;
+        $this->order->order_price = $order_price;
+        $this->order->vatvalue = $total_order_vat;
 
         //Set coupons
-        if($this->request->has('coupon_code')&&strlen($this->request->coupon_code)>0){
-            $coupon = Coupons::where(['code' => $this->request->coupon_code])->where('restaurant_id',$this->vendor->id)->get()->first();
-            if($coupon){
-                $deduct=$coupon->calculateDeduct($this->order->order_price);
-                if($deduct){
+        if ($this->request->has('coupon_code') && strlen($this->request->coupon_code) > 0) {
+            $coupon = Coupons::where(['code' => $this->request->coupon_code])->where('restaurant_id', $this->vendor->id)->get()->first();
+            if ($coupon) {
+                $deduct = $coupon->calculateDeduct($this->order->order_price);
+                if ($deduct) {
                     $coupon->decrement('limit_to_num_uses');
                     $coupon->increment('used_count');
-                    $this->order->coupon=$this->request->coupon_code;
-                    if($deduct>$this->order->order_price){
-                        $this->order->discount=$order_price;
+                    $this->order->coupon = $this->request->coupon_code;
+                    if ($deduct > $this->order->order_price) {
+                        $this->order->discount = $order_price;
 
                         //In this case, order should be considered as paid one
                         //$this->order->payment_status = 'paid';
-                    }else{
-                        $this->order->discount=$deduct;
+                    } else {
+                        $this->order->discount = $deduct;
                     }
-                    
                 }
             }
         }
-        
+
 
         //Update the order with the item
         $this->order->update();
     }
 
-    private function setComment(){
-       
-        $comment = $this->request->comment ? strip_tags($this->request->comment.'') : '';
-        $this->order->comment = $this->order->comment.' '.$comment;
+    private function setComment()
+    {
+
+        $comment = $this->request->comment ? strip_tags($this->request->comment . '') : '';
+        $this->order->comment = $this->order->comment . ' ' . $comment;
         $this->order->update();
     }
 
-    private function calculateFees(){
-        if($this->vendor){
-            $this->order->static_fee=$this->vendor->static_fee;
-            $this->order->fee=$this->vendor->fee;
-            $this->order->fee_value=($this->vendor->fee/100)*($this->order->order_price_with_discount-$this->vendor->static_fee);
+    private function calculateFees()
+    {
+        if ($this->vendor) {
+            $this->order->static_fee = $this->vendor->static_fee;
+            $this->order->fee = $this->vendor->fee;
+            $this->order->fee_value = ($this->vendor->fee / 100) * ($this->order->order_price_with_discount - $this->vendor->static_fee);
             $this->order->update();
         }
-        
     }
 
-    public function notifyAdmin(){
+    public function notifyAdmin()
+    {
         //Does nothing
     }
 
-    public function notifyOwner(){
-        //Inform owner - via email, sms or db
-        $this->vendor->user->notify((new OrderNotification($this->order,1,$this->vendor->user))->locale(strtolower(config('settings.app_locale'))));
+    public function notifyOwner()
+    {
+        //Inform owner - via email, sms, telegram or db
+        $orderNotification = new OrderNotification($this->order, 1, $this->vendor->user);
+        $this->vendor->user->notify(($orderNotification)->locale(strtolower(config('settings.app_locale'))));
+
+        //Notify owner with Telegram
+        $orderNotification->toTelegram();
 
         //Notify owner with pusher
         if (strlen(config('broadcasting.connections.pusher.secret')) > 4) {
